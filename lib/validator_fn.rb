487ab78a45f44fd1a17188f53b8d3969e8a71179
Martin Chabot <chabotm@gmail.com>
2020-12-24 09:48:39 -0500

    Adding Validator code

--- lib/validator_fn.rb
+++ lib/validator_fn.rb
@@ -1,6 +1,76 @@
 require "validator_fn/version"
+require "fn_reader"
 
 module ValidatorFn
   class Error < StandardError; end
-  # Your code goes here...
+
+  fn_reader :something, :matches, :either, :array_of, :any, :is_nil,
+            :maybe, :present, :is_a, :int, :hash_of, :invalid, :generate_validator, :handle_error, :error_str
+
+  class Error < StandardError; end
+
+  @@invalid = ->msg { raise Error.new(msg) }
+  @@something = ->a { invalid.("Cannot be nil") if a.nil?; a }
+  @@matches = ->regex, a { invalid.("Should match") unless a =~ regex }.curry
+  @@either = ->a, b, value {
+    begin
+      a.(value) || a.(value)
+    rescue Error => e
+    end
+    a
+  }.curry
+  @@array_of = ->fn, array {
+    is_a.(Array).(array)
+    array.each_with_index do |a, idx|
+      fn.(a)
+    rescue Error => e
+      invalid.("Invalid value in Array at index #{idx}")
+    end
+    array
+  }.curry
+  @@any = ->a { a }
+  @@is_nil = ->a { invalid.("Should be nil") unless a.nil?; a }
+  @@maybe = either.(is_nil)
+  @@is_a = ->klass, a { invalid.("Expected type #{klass}, got #{a.inspect}") unless a.is_a?(klass); a }.curry
+  @@int = ->a { invalid.("#{a.inspect} is not a valid integer") unless a.is_a?(Integer) }
+  @@hash_of = ->fields, hash {
+    hash ||= {}
+    fields.map do |(key, fn)|
+      [key, fn.(hash[key])]
+    rescue Error => e
+      invalid.("Invalid value for #{key.inspect} key.")
+    end.to_h
+  }.curry
+
+  @@handle_error = ->on_error, validator, value {
+    begin
+      validator.(value)
+    rescue Error => e
+      on_error.(e)
+    end
+  }.curry
+  @@error_str = ->idx, e {
+    if !e.nil?
+      indent = "  " * (idx + 1)
+      "#{e.message}\n" + indent + error_str.(idx + 1).(e.cause)
+    else
+      ""
+    end
+  }.curry
+
+  @@generate_validator = ->a {
+    case a
+    when nil
+      "any"
+    when Hash
+      inner = a.map do |k, v|
+        %{"#{k}" => #{generate_validator.(v)}}
+      end.join(",\n  ")
+      "hash_of.({ #{inner} })"
+    when Array
+      "array_of.( #{generate_validator.(a.first)} )"
+    else
+      "is_a.(#{a.class})"
+    end
+  }
 end
--- spec/validator_fn_spec.rb
+++ spec/validator_fn_spec.rb
@@ -1,9 +1,60 @@
+require "validator_fn"
+
 RSpec.describe ValidatorFn do
+  include ValidatorFn
   it "has a version number" do
     expect(ValidatorFn::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "validation" do
+    it "validates integer" do
+      expect { int.("Tata") }.to raise_error(ValidatorFn::Error)
+      expect { int.(12) }.not_to raise_error
+    end
+
+    it "validates non nil" do
+      expect { something.(nil) }.to raise_error(ValidatorFn::Error)
+      expect { something.(1) }.to_not raise_error
+    end
+
+    it "validates a has" do
+      expect { hash_of.(age: (int)).(age: "asdf") }.to raise_error(ValidatorFn::Error)
+      expect { hash_of.(age: (int)).(age: 12) }.to_not raise_error
+    end
+
+    it "validates an array" do
+      expect { array_of.(is_a.(String)).([]) }.to_not raise_error
+      expect { array_of.(is_a.(String)).([12]) }.to raise_error(ValidatorFn::Error)
+    end
+
+    it "validates a complex stucture" do
+      attachment = { "order_type" => "sent to carrier",
+                     "customer" => "swiftvox",
+                     "company_name" => nil,
+                     "first_name" => "eduardo prenda",
+                     "last_name" => nil,
+                     "dids" => "5147257161",
+                     "to_phone_num" => nil,
+                     "street_number" => "8842",
+                     "street_name" => "paul corbeil  ",
+                     "floor" => "", "suite" => "",
+                     "city" => "st-leonard",
+                     "postal_code" => "h1r 3a2",
+                     "province" => "qc",
+                     "local_provider" => nil,
+                     "reseller" => "",
+                     "created" => "2020-12-22",
+                     "duedate" => "2021-01-07",
+                     "account" => "",
+                     "comments" => nil,
+                     "pri" => ["swiftvox_1001"],
+                     "sales_comments" => nil, "status" => nil, "ispt_draft_id" => 1,
+                     "ispt_pon_id" => "137340", "ispt_customer_email" => "lnp@voip.ms", "reference" => "" }
+
+      validate_code = generate_validator.(attachment)
+      validate = eval(validate_code)
+      attachment["pri"] = [12]
+      handle_error.(error_str.(0)).(validate).(attachment)
+    end
   end
 end
